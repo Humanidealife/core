f89d96af05a23d45102847b1b5b075029281a770
WANG <wangqianlf@live.com>
2024-06-14 17:05:08 +0200

    création d'une propriété statique en int lastId, l'ajout de lastId et son incrémentation dans les constructeurs

--- src/main/java/com/directmedia/onlinestore/core/entity/Work.java
+++ src/main/java/com/directmedia/onlinestore/core/entity/Work.java
@@ -9,13 +9,22 @@ package com.directmedia.onlinestore.core.entity;
  * @author wangq
  */
 public class Work {
+    //On va ajouter comme ce qui est indiqué dans le sujet de l'Exo6, une propriété de tyep "int" et nommé "lastId"
+    //Ce "lastId" va avoir la valeur 0 pour commencer
+    private static int lastId;
     
     //Et un constructeur sans paramètre pour bien respecter le format de JavaBean
     public Work() {    
+        //On va faire en sorte qu'au moment de l'instanciation de l'oeuvre, "id" de l'oeuvre soit valorisé par la dernière valeur de
+        //"lastId". "lastId" vaut de départ 0, puis on l'incrémente immédiatement de 1. 
+        this.id = lastId++;
     }
 
     //On génère le constructeur paticulier qui prend le title en paramètre
     public Work(String title) {
+        //Attention ! Pour ce deuxième constructeur, on veut aussi que cela puisse associer un "id" à une "oeuvre".
+        //On pourrait répéter "this.id = lastId++", mais la façon la plus simple est de faire appel au constructeur sans paramètre
+        this();
         this.title = title;
     }
     
